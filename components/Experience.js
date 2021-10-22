3cf192395af34eacdc53b878cc063cf9d8b0a5b7
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2021-10-22 17:42:07 -0400

    Create your own file for the ExperienceCard component

--- components/Experience.js
+++ components/Experience.js
@@ -1,6 +1,6 @@
 import userData from '@constants/data';
-import React from 'react';
 import Title from '@components/Title';
+import ExperienceCard from '@components/ExperienceCard';
 
 export default function Experience() {
   return (
@@ -10,9 +10,8 @@ export default function Experience() {
         <div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-20">
           {/* Experience card */}
           {userData.experience.map((exp, idx) => (
-            <>
+            <div key={idx}>
               <ExperienceCard
-                key={idx}
                 title={exp.title}
                 desc={exp.desc}
                 year={exp.year}
@@ -22,30 +21,15 @@ export default function Experience() {
               {idx === userData.experience.length - 1 ? null : (
                 <div className="divider-container flex flex-col items-center -mt-2">
                   <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
-                    <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+                    <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping" />
                   </div>
-                  <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+                  <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2" />
                 </div>
               )}
-            </>
+            </div>
           ))}
         </div>
       </div>
     </section>
   );
 }
-
-const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
-  return (
-    <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
-      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
-        {year}
-      </h1>
-      <h1 className="font-semibold text-xl">{title}</h1>
-      <a href={companyLink} className="text-gray-500">
-        {company}
-      </a>
-      <p className="text-gray-600 dark:text-gray-400 my-2">{desc}</p>
-    </div>
-  );
-};
