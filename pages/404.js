1583f6a9f0033b35cb9c2393d13f629d2fa1be51
Jorge de Jesús Rodríguez Díaz <jrodriguezdiazz@outlook.com>
2021-11-13 12:00:37 -0400

    Add pages for 404 and 500 errors (#2)

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import ContainerBlock from '../components/ContainerBlock';
+import Component404 from '@components/Error404';
+
+export default function Custom404() {
+  return (
+    <ContainerBlock title="404 ~ Jorge Rodríguez">
+      <Component404 />
+    </ContainerBlock>
+  );
+}
---
+++ pages/500.js
@@ -0,0 +1,10 @@
+import ContainerBlock from '../components/ContainerBlock';
+import Component500 from '@components/Error500';
+
+export default function Custom404() {
+  return (
+    <ContainerBlock title="500 ~ Jorge Rodríguez">
+      <Component500 />
+    </ContainerBlock>
+  );
+}
